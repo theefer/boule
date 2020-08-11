a9929be15452ce970c536a958cc282a2a96ec349
Sébastien Cevey <seb@cine7.net>
2020-08-11 15:02:01 +0100

    Upgrade to latest sapper and other libs

--- src/server.js
+++ src/server.js
@@ -6,14 +6,12 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
-
-export default app;
+polka()
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
